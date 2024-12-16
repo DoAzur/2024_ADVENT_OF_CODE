3d1bef50deef46d020fd665d750ba47198c9bd54
Nickolay Borschev <myonetelephon@gmail.com>
2024-12-16 05:04:17 +0300

    all

---
+++ task1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void read_arrays(FILE *input, int **A, int**B, int *size)
+{
+    int length = 1; //текущее выделение памяти относительно длины length
+    int t = 2;
+    int element_A = 0;
+    int element_B = 0;
+    *size = 0; //длина итогого массива
+    *A = malloc(length * sizeof(int));
+    *B = malloc(length * sizeof(int));
+    t = fscanf(input, "%d %d", &element_A, &element_B);
+    while (t == 2) {
+        if (*size >= length) {
+            length *= 2;
+            int *temp1 = realloc(*A, length * sizeof(int));
+            *A = temp1;
+            int *temp2 = realloc(*B, length * sizeof(int));
+            *B = temp2;
+        }
+        (*A)[*size] = element_A;
+        (*B)[*size] = element_B;
+        (*size)++;
+        t = fscanf(input, "%d %d", &element_A, &element_B);
+    }
+}
+void buble_sort(int *array, int size){
+    int temp;
+    for (int i = 0; i < size - 1; i++){
+        for (int j = 0; j < size - 1; j++){
+            if (array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+int main(int argc, char *argv[])
+{
+    int *A = NULL;
+    int *B = NULL;
+    int size = 0;
+    int ans = 0;
+    FILE *input = NULL;
+    if (argc != 2) {
+        printf("error\n");
+        return -1;
+    }
+    input = fopen(argv[1], "r");
+    if (input == NULL) {
+        printf("error\n");
+        return -1;
+    }
+    read_arrays(input, &A, &B, &size);
+    fclose(input);
+    buble_sort(A, size);
+    buble_sort(B, size);
+    for (int i = 0; i < size; i++){
+        ans += fabs(A[i] - B[i]);
+    }
+    printf("%d\n", ans);
+    free(A);
+    free(B);
+    return 0;
+}
